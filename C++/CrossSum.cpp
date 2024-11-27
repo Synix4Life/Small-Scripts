7ed718cf1727e309b14e06b5e15a63d194121300
Synix <nt.lang2002@gmail.com>
2024-11-27 17:50:04 +0100

    =0
    Just SOMEHOW keep my streak up LOOOL uff

--- C++/CrossSum.cpp
+++ C++/CrossSum.cpp
@@ -1,5 +1,8 @@
-#include <iostream>
-
+/**
+ * Method to calculate the Cross Sum
+ * @param value The value
+ * @return The Cross Sum
+ */
 int cross_sum(int value){
     int result = 0;
     while(value > 0){
@@ -7,11 +10,4 @@ int cross_sum(int value){
         result /= 10;
     }
     return result;
-}
-
-int main(int argc, char* argv[]){
-    int in;
-    std::cout << "Please enter an Integer: ";
-    std::cin >> in;
-    std:: cout << "For " << in << ", we got " << cross_sum(in) << std::endl;
 }
